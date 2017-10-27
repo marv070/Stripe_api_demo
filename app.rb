d364fcd47b1b15fe61e1eee3e709f70076c11910
Marvin Laucher <marv070@gmail.com>
2017-10-27 13:50:51 -0400

    stripe api demo app

--- app.rb
+++ app.rb
@@ -1,31 +1,39 @@
 require 'sinatra'
-require 'sinatra/reloader' if development?
 require 'stripe'
 
-Stripe.api_key = 'sk_test_***' # Paste your secret key here. https://dashboard.stripe.com/account/apikeys
+set :publishable_key, 'pk_test_Z1gfhNPlVZWZ0MzRZigwECWN'
+set :secret_key, 'sk_test_Vfkn4yYppHb5XBCaLEcGARhf'
+Stripe.api_key = settings.secret_key
+
 
 get '/' do
   erb :index
 end
 
-get '/somethingelse' do
-  "This is another page"
+post '/charge' do
+  # Amount in cents
+  @amount = 10000
+
+  customer = Stripe::Customer.create(
+    :email => 'customer@example.com',
+    :source  => params[:stripeToken]
+  )
+
+  charge = Stripe::Charge.create(
+    :amount      => @amount,
+    :description => 'Sinatra Charge',
+    :currency    => 'usd',
+    :customer    => customer.id
+  )
+
+  erb :charge
 end
 
-post '/charge' do
-  # Get the credit card details submitted by the form
-  token = params[:stripeToken]
-  email = params[:stripeEmail]
-  # Create the charge on Stripe's servers - this will charge the user's card
-  begin
-    charge = Stripe::Charge.create(
-      :amount => 2000, # amount in cents, again
-      :currency => "usd",
-      :source => token,
-      :description => email
-    )
-    "Successfully charged"
-  rescue Stripe::CardError => e
-    "Your card was declined" # The card has been declined, use 4100000000000019 to test in Checkout
-  end
+error Stripe::CardError do
+  env['sinatra.error'].message
 end
+
+
+
+
+
